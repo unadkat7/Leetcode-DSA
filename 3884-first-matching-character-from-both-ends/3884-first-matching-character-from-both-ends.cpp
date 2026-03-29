9c52a24648d2ab6e250207537b5f6de994d6bc79
Jay Unadkat <153261041+unadkat7@users.noreply.github.com>
2026-03-29 15:45:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (31.58%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<=j){
+            if(s[i]==s[j]){
+                return i;
+            }
+            i++;
+            j--;
+        }
+
+        return -1;
+    }
+};
