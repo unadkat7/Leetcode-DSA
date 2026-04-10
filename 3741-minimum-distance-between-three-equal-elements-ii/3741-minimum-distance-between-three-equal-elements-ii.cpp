e74663fa162da176626e06b3194abea6a6cd5516
Jay Unadkat <153261041+unadkat7@users.noreply.github.com>
2026-04-10 16:18:52 +0530

    Time: 500 ms (45.8%), Space: 381.7 MB (39.11%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int,vector<int>> mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        int ans = INT_MAX;
+        for(auto &it:mp){
+            vector<int> v=it.second;
+
+            if(v.size()<3) continue;
+
+            for(int i=0;i<v.size()-2;i++){
+                int dist = 2*(v[i+2] - v[i]);
+                ans = min(ans,dist); 
+            }
+        }
+
+        return ans == INT_MAX? -1 : ans;
+    }
+};
